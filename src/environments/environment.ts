acc9772956145c9d33f4cb1a4857b9e16b3c3452
Carol Tatiana Giraldo Hernandez <tec_carolg@A45206.confamiliares.com>
2024-11-08 11:48:21 -0500

    Comentar letrero pruebas y rutas de producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,15 @@
 export const environment = {
   production: false,
   identificacionFacial:
-    'https://app.confa.co:8687/transaccionAutenticacionWS/transaccion/',
+    'https://identidad.confa.co/transaccionAutenticacionWS/',
   consultarIp: 'https://appint.confa.co/consultaIP/circular007/obtenerIP/',
   consultaLambdaDirecta: 'https://api-facial.confa.co/identificarvalidar',
   excepcionFacial: 'ojos-boca-dimension-gafasDeSol-brillo',
   tipoValidacionFacial: 'validacion',
 // identificacionSas: 'https://app.confa.co:8361/medicamentosWS/rest/medicamentos/', // ruta apuntando al nbappo
   //identificacionSas: 'http://localhost:8081/medicamentosWS/rest/medicamentos/',
-  identificacionSas: 'https://sasweb.confa.co:8322/medicamentosWS/rest/medicamentos/', //ruta apuntando al DMZSAS
+  //identificacionSas: 'https://sasweb.confa.co:8322/medicamentosWS/rest/medicamentos/', //ruta apuntando al DMZSAS
+  identificacionSas: 'https://sasweb.confa.co:8586/medicamentosWS/rest/medicamentos/', // producción
   parametro1: "hlZTM4ZDcwNDRlODcyNzZDX1BPUlQqMjAxOCQ=", 
   parametro2: "UG9ydGFsX0NvbmZhODRkZGZiMzQxMjZmYzNhND",
   apiIngresoConfa: "https://app.confa.co:8687/ingresoConfaWSSGC/rest/", //ruta de pruebas apuntando a genesys
